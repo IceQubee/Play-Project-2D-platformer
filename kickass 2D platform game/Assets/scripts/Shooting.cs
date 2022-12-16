493e04a5b9787173a29341561a5d8c6ddb122a9d
Stan van Berkel <36487239+IceQubee@users.noreply.github.com>
2022-12-16 21:16:22 +0100

    S flips character + LMB spawns bullet
    s key now flips transform rotation of player (also the position where bullets are fired from bc that position in parented to player) and LMB now spawns bullets prefabs without velocity

---
+++ kickass 2D platform game/Assets/scripts/Shooting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
+        }
+    }
+}
